8e23b25c3f99bb44657612a112ccb5ef8dc44e11
Anandawd <75649238+Anandawd@users.noreply.github.com>
2023-06-08 01:04:52 +0700

    Add files via upload

--- Dounat Catchers/deps/include/GLM/extend.hpp
+++ Dounat Catchers/deps/include/GLM/extend.hpp
@@ -24,59 +24,44 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 ///
-/// @ref gtx_common
-/// @file glm/gtx/common.hpp
-/// @date 2014-09-08 / 2014-09-08
+/// @ref gtx_extend
+/// @file glm/gtx/extend.hpp
+/// @date 2006-01-07 / 2011-06-07
 /// @author Christophe Riccio
 ///
 /// @see core (dependence)
-/// @see gtc_half_float (dependence)
 ///
-/// @defgroup gtx_common GLM_GTX_common
+/// @defgroup gtx_extend GLM_GTX_extend
 /// @ingroup gtx
 /// 
-/// @brief Provide functions to increase the compatibility with Cg and HLSL languages
+/// @brief Extend a position from a source to a position at a defined length.
 /// 
-/// <glm/gtx/common.hpp> need to be included to use these functionalities.
+/// <glm/gtx/extend.hpp> need to be included to use these functionalities.
 ///////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
 
-// Dependencies:
-#include "../vec2.hpp"
-#include "../vec3.hpp"
-#include "../vec4.hpp"
-#include "../gtc/vec1.hpp"
+// Dependency:
+#include "../glm.hpp"
 
 #if(defined(GLM_MESSAGES) && !defined(GLM_EXT_INCLUDED))
-#	pragma message("GLM: GLM_GTX_common extension included")
+#	pragma message("GLM: GLM_GTX_extend extension included")
 #endif
 
 namespace glm
 {
-	/// @addtogroup gtx_common
+	/// @addtogroup gtx_extend
 	/// @{
 
-	/// Returns true if x is a denormalized number
-	/// Numbers whose absolute value is too small to be represented in the normal format are represented in an alternate, denormalized format.
-	/// This format is less precise but can represent values closer to zero.
-	/// 
-	/// @tparam genType Floating-point scalar or vector types.
-	///
-	/// @see <a href="http://www.opengl.org/sdk/docs/manglsl/xhtml/isnan.xml">GLSL isnan man page</a>
-	/// @see <a href="http://www.opengl.org/registry/doc/GLSLangSpec.4.20.8.pdf">GLSL 4.20.8 specification, section 8.3 Common Functions</a>
+	/// Extends of Length the Origin position using the (Source - Origin) direction.
+	/// @see gtx_extend
 	template <typename genType> 
-	GLM_FUNC_DECL typename genType::bool_type isdenormal(genType const & x);
-
-	/// Similiar to 'mod' but with a different rounding and integer support.
-	/// Returns 'x - y * trunc(x/y)' instead of 'x - y * floor(x/y)'
-	/// 
-	/// @see <a href="http://stackoverflow.com/questions/7610631/glsl-mod-vs-hlsl-fmod">GLSL mod vs HLSL fmod</a>
-	/// @see <a href="http://www.opengl.org/sdk/docs/manglsl/xhtml/mod.xml">GLSL mod man page</a>
-	template <typename T, precision P, template <typename, precision> class vecType>
-	GLM_FUNC_DECL vecType<T, P> fmod(vecType<T, P> const & v);
+	GLM_FUNC_DECL genType extend(
+		genType const & Origin, 
+		genType const & Source, 
+		typename genType::value_type const Length);
 
 	/// @}
 }//namespace glm
 
-#include "common.inl"
+#include "extend.inl"
